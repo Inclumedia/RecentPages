efb736eeaab85af7d488b9bc4513229549d47eb4
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-01-22 10:52:11 +0100

    Create RecentPages.i18n.php
    * Moved English message to this file
    * Added German message

---
+++ RecentPages.i18n.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Internationalization file for the RecentPages extension.
+ *
+ * @licence GPL v2 or later
+ * @author Karsten Hoffmeyer (Kghbln)
+ */
+
+$messages = array();
+
+/** English
+ * @author Leucosticte
+ * @author Kghbln
+ */
+$messages['en'] = array(
+    'rp-desc' => 'Allows to list recently created or random pages'
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+    'rc-desc' => 'Ermöglicht das Auflisten von kürzlich erstellen oder zufälligen Seiten'
+);
